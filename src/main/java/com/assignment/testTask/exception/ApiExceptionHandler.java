15b20b733c2913f24f77209f24d8af287912afad
forgjayjay <oneitswarrior@gmail.com>
2024-04-29 20:28:04 +0200

    added handling for error 404 and type mismatch

--- src/main/java/com/assignment/testTask/exception/ApiExceptionHandler.java
+++ src/main/java/com/assignment/testTask/exception/ApiExceptionHandler.java
@@ -7,14 +7,25 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class ApiExceptionHandler{
+    
+    public static final String DEFAULT_ERROR_VIEW = "error";
     
     @ExceptionHandler(value = {ApiRequestException.class})
     public ResponseEntity<?> handleApiRequestException(ApiRequestException e){
         HttpStatusCode badRequest = HttpStatusCode.valueOf(400);
         ApiException apiException = new ApiException(e.getMessage(), badRequest, ZonedDateTime.now(ZoneId.of("Z")));
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> defaultErrorHandler(Exception e){
+        HttpStatusCode badRequest = HttpStatusCode.valueOf(400);
+        ApiException apiException = new ApiException("Wrong type input", badRequest, ZonedDateTime.now(ZoneId.of("Z")));
         return new ResponseEntity<>(apiException, badRequest);
     }
 
@@ -24,4 +35,12 @@ public class ApiExceptionHandler{
         ApiException apiException = new ApiException(e.getMessage(), internalServerError, ZonedDateTime.now(ZoneId.of("Z")));
         return new ResponseEntity<>(apiException, internalServerError);
     }
+
+    @ExceptionHandler(value = {NoResourceFoundException.class})
+    public ResponseEntity<?> handleNoResourceFoundException(NoResourceFoundException e){
+        HttpStatusCode noResourceFound = HttpStatusCode.valueOf(404);
+        ApiException apiException = new ApiException("No resource found", noResourceFound, ZonedDateTime.now(ZoneId.of("Z")));
+        return new ResponseEntity<>(apiException, noResourceFound);
+    }
+
 }
